3c97fbe8c6c0e5cfe77ac8f77cef3e1f6f0b735b
slowmike <aznwizard123@gmail.com>
2018-06-13 12:39:27 -0700

    functional completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,26 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if(size > 0) {
+      var nextVal = storage[0];
+      for(var i = 1; i < size; i++) {
+        storage[i-1] = storage[i];
+      }
+      delete storage[size-1];
+      size--;
+      return nextVal;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,23 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if(size > 0) {
+      var topVal = storage[size-1];
+      delete storage[size-1];
+      size--;
+      return topVal;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
