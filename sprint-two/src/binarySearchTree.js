25acd759f39f1c4b8e75b16867819563e19daa70
slowmike <aznwizard123@gmail.com>
2018-06-16 14:48:24 -0700

    commented and analysed bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -10,24 +10,43 @@ var BinarySearchTree = function(value) {
 
 var binaryTreeMethods = {};
 
+/**
+ * insert(num)
+ * Input: a number to add to the BST
+ * Output: none
+ * Side Effects: using the Binary Tree rule, num is added as a child to the tree
+ * Constraints: num cannot be null and cannot already exist in the BST
+ * Time Complexity: O(log n)
+ */
 binaryTreeMethods.insert = function(num) {
 
   //the tree is populated with some item
-  if (num < this.value && num !== null) {
-    if (this.left !== undefined) {
-      this.left.insert(num);
+  if (num !== null) {
+    if (num < this.value) {
+      if (this.left !== undefined) {
+        this.left.insert(num);
+      } else {
+        this.left = BinarySearchTree(num);
+      }
     } else {
-      this.left = BinarySearchTree(num);
-    }
-  } else {
-    if (this.right !== undefined) {
-      this.right.insert(num);
-    } else {
-      this.right = BinarySearchTree(num);
+      if (this.right !== undefined) {
+        this.right.insert(num);
+      } else {
+        this.right = BinarySearchTree(num);
+      }
     }
   }
+
 };
 
+/**
+ * contains(num)
+ * Input: a number to be found in the BST
+ * Output: A boolean indicating if num was found in the BST
+ * Side Effects: none
+ * Constraints: num cannot be null
+ * Time Complexity: O(log n)
+ */
 binaryTreeMethods.contains = function(num) {
   if (num < this.value) {
     if (this.left !== undefined) {
@@ -43,6 +62,14 @@ binaryTreeMethods.contains = function(num) {
   return this.value === num;
 };
 
+/**
+ * depthFirstLog(cb)
+ * Input: a callback function
+ * Output: none
+ * Side Effects: invokes the callback on every node in the tree, in a DFS order
+ * Constraints: callback cannot be null
+ * Time Complexity: O(n)
+ */
 binaryTreeMethods.depthFirstLog = function(cb) {
   cb(this.value);
 
